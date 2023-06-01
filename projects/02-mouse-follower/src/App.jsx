1bce7dc19e74881302472702f2a306015ebb7c39
sxngg <santiagogarciagarzon.8@gmail.com>
2023-06-01 17:26:02 -0500

    creating new project mouse-follower

---
+++ projects/02-mouse-follower/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  /**
+   * * State para cambiar botón de Activar a Desactivar
+   */
+  const [enabled, setEnabled] = useState(false);
+
+  /** 
+   * * useEffect que ejecutaremos cada que cambie el estado de enabled 
+   */
+
+  useEffect(()=>{
+    console.log('effect',{enabled})
+  },[enabled])
+
+  return (
+    <>
+    <h1>
+       3
+    </h1>
+    
+    <button onClick={()=>{
+      setEnabled(!enabled)
+    }}>
+      {enabled ? 'Activar' : 'Desactivar'}
+    </button>
+    </>
+  )
+}
+
+export default App
