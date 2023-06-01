1909dd74aa4aa2c089a4c685742b2924205133db
sxngg <santiagogarciagarzon.8@gmail.com>
2023-05-31 23:09:02 -0500

    board for the game is created

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return <h1>Tic tac toe</h1>
+
+  //Creamos un useState para manejar el tablero de juego, este va a ser un array de 9 elementos (todos null)
+  const board = useState(Array(9).fill(null));
+  
+  return (
+    
+    
+    <main className='board'>
+      <h1>Tic tac toe</h1>
+      <section className='game'>
+        {
+          board.map((_,index)=>{
+            return (
+              <div className='cell' key={index}>
+                <span className='cell_content'>
+                  {index}
+                </span>
+              </div>
+            )
+          })
+        }
+      </section>
+    </main>
+  ) 
 }
 
 export default App
