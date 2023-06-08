db089b119e0021259a0a9d4dd58476e9b468a376
sxngg <santiagogarciagarzon.8@gmail.com>
2023-06-07 21:05:28 -0500

    feat(App): Using cat image api, we give three first words of a fact and api give us a image with those words

--- projects/03-technical-test/src/App.jsx
+++ projects/03-technical-test/src/App.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react'
+import './App.css'
 
 export function App () {
   const CAT_FACT_API = 'https://catfact.ninja/fact'
-  const [fact, setFact] = useState('')
+  const CAT_IMAGE_URL = 'https://cataas.com'
+  const [fact, setFact] = useState()
+  const [catImageUrl, setCatImageUrl] = useState()
 
   useEffect(() => {
     fetch(CAT_FACT_API)
@@ -10,15 +13,29 @@ export function App () {
       .then(data => {
         const { fact } = data
         setFact(fact)
-
-        const firstWord = fact.split(' ', 1)
-        console.log(firstWord)
       })
   }, [])
+
+  useEffect(() => {
+    if (!fact) return
+    const threeFirstWords = fact.split(' ', 3)
+    console.log(threeFirstWords.join(' '))
+
+    fetch(`${CAT_IMAGE_URL}/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(data => {
+        const { url } = data
+        setCatImageUrl(url)
+      })
+  }, [fact])
+
   return (
-    <>
+    <main>
       <h1>Cat Facts</h1>
-      <p> {fact} </p>
-    </>
+      <section>
+        <p> {fact} </p>
+        <p>{catImageUrl && <img src={`${CAT_IMAGE_URL}${catImageUrl}`} alt={`Image extracted with three first words in the API: ${CAT_IMAGE_URL}`} />} </p>
+      </section>
+    </main>
   )
 }
