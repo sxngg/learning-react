ec805d0220ad44f57c27c2d0352be9236a542adb
sxngg <santiagogarciagarzon.8@gmail.com>
2023-06-07 22:55:14 -0500

    Implement a button for get a new random fact and the code is destructured from App and create folder services and the file randomFact

--- projects/03-technical-test/src/App.jsx
+++ projects/03-technical-test/src/App.jsx
@@ -1,19 +1,14 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { getRandomFact } from './services/randomFact'
 
 export function App () {
-  const CAT_FACT_API = 'https://catfact.ninja/fact'
   const CAT_IMAGE_URL = 'https://cataas.com'
   const [fact, setFact] = useState()
   const [catImageUrl, setCatImageUrl] = useState()
 
   useEffect(() => {
-    fetch(CAT_FACT_API)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      })
+    getRandomFact().then(newFact => setFact(newFact))
   }, [])
 
   useEffect(() => {
@@ -29,9 +24,16 @@ export function App () {
       })
   }, [fact])
 
+  async function handleClick () {
+    // getRandomFact().then(newFact => setFact(newFact))
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
+
   return (
     <main>
       <h1>Cat Facts</h1>
+      <button onClick={handleClick}> Get random fact </button>
       <section>
         <p> {fact} </p>
         <p>{catImageUrl && <img src={`${CAT_IMAGE_URL}${catImageUrl}`} alt={`Image extracted with three first words in the API: ${CAT_IMAGE_URL}`} />} </p>
